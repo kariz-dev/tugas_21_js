a279124af6245d9a2ad88b74b8be46ab84fd999b
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 22:10:00 +0700

    tugas 21 Muhammad Rizwan

---
+++ tugas21.js
@@ -0,0 +1,13 @@
+/* Tugas 21 Javascrript */
+// Muhammad Rizwan
+
+function tugasDuaPuluhSatu(){
+    var kata = "saya belajar masak nasi goreng";
+    
+    console.log(kata);
+    
+    var rubah = kata.replace("masak","makan");
+    return rubah;
+}
+
+console.log(tugasDuaPuluhSatu());
